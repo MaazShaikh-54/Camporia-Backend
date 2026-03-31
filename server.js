1e24e3ada0038a5fda797da137b6cab4e152df05
Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2026-04-01 00:58:24 +0530

    port updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -17,5 +17,5 @@ mongoose
 
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server runnning on port ${PORT}`));
