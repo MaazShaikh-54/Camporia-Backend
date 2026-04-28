0ede4361e036ee453c91fbb114dd0f8aa8c2aa2a
Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2026-04-29 03:29:08 +0530

    Fox: Admin-User MVC import error

--- routes/admin/adminUserRoutes.js
+++ routes/admin/adminUserRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authMiddleware, { adminOnly } from "../../middleware/authMiddleware.js";
+import authMiddleware, { adminOnly } from "../../middleware/auth.js";
 import {
     getUsers,
     updateUser,
