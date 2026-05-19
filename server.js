c8a07195972f3f7b01dbf240bc6ebb5c0f44e592
Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2026-05-20 01:42:43 +0530

    Add: Journey admin routes

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import couponRoutes from "./routes/couponRoutes.js";
 //admin router
 import adminUserRoutes from "./routes/admin/adminUserRoutes.js";
 import adminCampsiteRoutes from "./routes/admin/adminCampsiteRoutes.js";
+import adminJourneyRoutes from "./routes/admin/adminJourneyRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -33,6 +34,7 @@ app.use("/coupon", couponRoutes)
 //admin routes
 app.use("/admin", adminUserRoutes);
 app.use("/admin", adminCampsiteRoutes);
+app.use("/admin", adminJourneyRoutes);
 
 const PORT = process.env.PORT || 10000;
 
