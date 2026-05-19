ca61d7b52b682feffeb60078dcb70f17285576a7
Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2026-05-20 02:11:22 +0530

    Fix: middleware import in journeyroute

--- routes/journeyRoutes.js
+++ routes/journeyRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authMiddleware, { adminOnly } from "../../middleware/auth.js";
+import authMiddleware, { adminOnly } from "../middleware/auth.js";
 import {
   createJourney,
   getJourneys,
